492129df3434b725d1882773a7155a04a7748f2b
Arkaedan <sam@samharris.dev>
2022-12-20 09:09:14 +1000

    Day 1

---
+++ 1/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+
+var elves = new List<int>();
+
+elves.Add(0);
+int elfCount = 0;
+
+foreach (var line in lines)
+{
+    if (String.IsNullOrWhiteSpace(line))
+    {
+        elves.Add(0);
+        elfCount++;
+    }
+    else
+    {
+        elves[elfCount] += int.Parse(line.Trim());
+    }
+}
+
+var sortedElves = elves.OrderByDescending(x => x).ToList();
+
+int topThree = sortedElves[0] + sortedElves[1] + sortedElves[2];
+
+Console.WriteLine($"Top One: {sortedElves[0]}");
+Console.WriteLine($"Top Three: {topThree}");
